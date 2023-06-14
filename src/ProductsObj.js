720951a9221cd4ecdf6a843ac57d0512dc658dc8
AyahDweikat <dweikatayah99@gmail.com>
2023-06-14 18:06:37 +0300

    product folder

---
+++ src/ProductsObj.js
@@ -0,0 +1,77 @@
+const products= [
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"https://res.cloudinary.com/dujgkfw6w/image/upload/v1686752319/saraha/user/i1vxbienbzbl2hzv9rwv.jpg",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"https://res.cloudinary.com/dujgkfw6w/image/upload/v1686752319/saraha/user/i1vxbienbzbl2hzv9rwv.jpg",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"https://res.cloudinary.com/dujgkfw6w/image/upload/v1686752319/saraha/user/i1vxbienbzbl2hzv9rwv.jpg",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"https://res.cloudinary.com/dujgkfw6w/image/upload/v1686752319/saraha/user/i1vxbienbzbl2hzv9rwv.jpg",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"https://res.cloudinary.com/dujgkfw6w/image/upload/v1686752319/saraha/user/i1vxbienbzbl2hzv9rwv.jpg",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"https://res.cloudinary.com/dujgkfw6w/image/upload/v1686752319/saraha/user/i1vxbienbzbl2hzv9rwv.jpg",
+        price:"20",
+        unitOfMeasure:"item"
+    },
+    {
+        name:"bag",
+        code:"123456",
+        category:"Woman Accessories",
+        image:"https://res.cloudinary.com/dujgkfw6w/image/upload/v1686752319/saraha/user/i1vxbienbzbl2hzv9rwv.jpg",
+        price:"20",
+        unitOfMeasure:"item"
+    }
+]
+
+
+export default products;
