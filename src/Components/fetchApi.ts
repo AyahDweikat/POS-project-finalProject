d693be7e04cc6701b5b73fc841cce9c019232eec
AyahDweikat <dweikatayah99@gmail.com>
2023-06-20 16:51:30 +0300

    --

--- src/Components/fetchApi.ts
+++ src/Components/fetchApi.ts
@@ -10,8 +10,6 @@ interface loginObj {
     email: string;
     password: string;
 }
-
-
 export const fetchApi =async (method:string,obj:(signUpObj | loginObj), url:string)=>{
     const requestOptions = {
         method,
@@ -22,12 +20,7 @@ export const fetchApi =async (method:string,obj:(signUpObj | loginObj), url:stri
         url,
         requestOptions
     ).then((response) => response.json())
-    // return results;
 }
-
-
-
-
 export const fetchApiWithAuthNoBody =async (method:string, url:string, token:string)=>{
     const requestOptions = {
         method,
@@ -37,10 +30,7 @@ export const fetchApiWithAuthNoBody =async (method:string, url:string, token:str
         url,
         requestOptions
     ).then((response) => response.json())
-    // return results;
 }
-
-
 export const fetchApiWithAuthAndBody =async (method:string,obj={}, url:string, token:string)=>{
     const requestOptions = {
         method,
@@ -51,28 +41,9 @@ export const fetchApiWithAuthAndBody =async (method:string,obj={}, url:string, t
         url,
         requestOptions
     ).then((response) => response.json())
-    // return results;
 }
-// export const fetchApiWithAuthDelete =async (method:string, url:string, token:string)=>{
-//     const requestOptions = {
-//         method,
-//         headers: { "Content-Type": "application/json", "authorization": token},
-//       };
-//     return fetch(
-//         url,
-//         requestOptions
-//     ).then((response) => response.json())
-//     // return results;
-// }
-// const requestOptions = {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(task),
-//   };
-//   fetch(
-//     "https://todolist-backend-app-nodb.onrender.com/addTasks",
-//     requestOptions
-//   )
-//     .then((response) => response.json())
-//     .then((data) => data);
-//   setAllTasks([...allTasks, task]);
+
+
+
+
+
