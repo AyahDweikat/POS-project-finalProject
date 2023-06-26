62322b920187c706465b4268565a44ca61d55cb0
AyahDweikat <dweikatayah99@gmail.com>
2023-06-27 02:00:23 +0300

    navbar cleaning code and add some css

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,30 +1,33 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import {Box} from '@mui/material';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-// import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-// import MenuItem from '@mui/material/MenuItem';
-// import AdbIcon from '@mui/icons-material/Adb';
-import {Form, useNavigate} from 'react-router-dom'
-import { useContext } from 'react';
-import { GlobalContext } from '../../Context/context.tsx';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import { Box } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import { Form, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { GlobalContext } from "../../Context/context.tsx";
+import styles from "./navbar.module.css";
 
-
-const settings = ['Profile', 'Logout'];
+const settings = ["Profile", "Logout"];
 
 function Navbar() {
-  const { auth } = useContext(GlobalContext)
-  const pages = auth?.token ? ['Carts', 'Products', 'Categories', 'Units'] :['Signup', 'login'] 
-  const navigate = useNavigate()
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const { auth } = useContext(GlobalContext);
+  const pages = auth?.token
+    ? ["Carts", "Products", "Categories", "Units"]
+    : ["Signup", "login"];
+  const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -42,7 +45,7 @@ function Navbar() {
   };
 
   return (
-    <AppBar sx={{backgroundColor:"black"}} position="static">
+    <AppBar sx={{ backgroundColor: "black" }} position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
@@ -50,19 +53,15 @@ function Navbar() {
             noWrap
             component="a"
             href="/"
+            className={styles.navbar}
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              display: { xs: "none", md: "flex" },
             }}
           >
             POS
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -77,29 +76,34 @@ function Navbar() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
                 <Form action={`${page}`} key={page}>
-                    <Button
-                      type="submit"
-                      onClick={handleCloseNavMenu}
-                      sx={{ p: 2, color: 'black', display: 'block', width:"100%" }}
-                      >
-                      {page}
-                    </Button>
+                  <Button
+                    type="submit"
+                    onClick={handleCloseNavMenu}
+                    sx={{
+                      p: 2,
+                      color: "black",
+                      display: "block",
+                      width: "100%",
+                    }}
+                  >
+                    {page}
+                  </Button>
                 </Form>
               ))}
             </Menu>
@@ -109,78 +113,78 @@ function Navbar() {
             noWrap
             component="a"
             href=""
+            className={styles.navbar}
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
             }}
           >
             POS
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-                <Form action={`${page}`} key={page}>
-                    <Button
-                      type="submit"
-                      onClick={handleCloseNavMenu}
-                      sx={{ my: 2, color: 'white', display: 'block' }}
-                      >
-                      {page}
-                    </Button>
-                </Form>
+              <Form action={`${page}`} key={page}>
+                <Button
+                  type="submit"
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              </Form>
             ))}
           </Box>
-          {auth?.token &&  
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <MenuIcon sx={{color:"white"}} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <Form  key={setting}>
-                <Button
-                  // type="submit"
-                  onClick={()=>{
-                    handleCloseNavMenu()
-                    if(setting == "Logout"){
-                      navigate('/login')
-                      auth.signout()
-                      localStorage.clear();
-                    } else {
-                      navigate('/profile')
-                    }
-                  }}
-                  sx={{ p: 2, color: 'black', display: 'block', width:"100%" }}
-                  >
-                  {setting}
-                </Button>
-            </Form>
-              ))}
-            </Menu>
-          </Box>
-          }
+          {auth?.token && (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <MenuIcon sx={{ color: "white" }} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <Form key={setting}>
+                    <Button
+                      onClick={() => {
+                        handleCloseNavMenu();
+                        if (setting == "Logout") {
+                          navigate("/login");
+                          auth.signout();
+                          localStorage.clear();
+                        } else {
+                          navigate("/profile");
+                        }
+                      }}
+                      sx={{
+                        p: 2,
+                        color: "black",
+                        display: "block",
+                        width: "100%",
+                      }}
+                    >
+                      {setting}
+                    </Button>
+                  </Form>
+                ))}
+              </Menu>
+            </Box>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
