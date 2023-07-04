fbc5c16413acacfe4b62938047468a1a41d605f7
AyahDweikat <dweikatayah99@gmail.com>
2023-07-04 23:03:56 +0300

    remove unnecessary lines

--- src/SubComponents/snackbar.test.tsx
+++ src/SubComponents/snackbar.test.tsx
@@ -1,11 +1,9 @@
-import { describe, test, expect, vi } from "vitest";
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { describe, test, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { beforeEach } from "vitest";
 import SnackbarComponent from "./Snackbar";
-// const token =
-//   "black__eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTBhOGI3OTBlZmUxYzAzOWUwZmIzNiIsImlhdCI6MTY4NzI1NzU5NywiZXhwIjoxNjg3MzQzOTk3fQ.Oiem-z2e6MJAfBlMCYHKmzQqE6pT_2dsDFIxgFqOx60";
 
-describe("Categories Page", () => {
+describe("Snackbar Component", () => {
   beforeEach(() => {
     render(<SnackbarComponent snackBarMsg="hello It's snackbar" />);
   });
